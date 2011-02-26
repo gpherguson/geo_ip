0055e87b636d809425659678e8e46d2c4152aced
gpherguson <8si.greg@gmail.com>
2011-02-25 22:22:24 -0700

    converted to use Minitest

--- test/test_geo_ip.rb
+++ test/test_geo_ip.rb
@@ -1,77 +1,69 @@
-#!/usr/bin/bacon
+require 'minitest/autorun'
 
-# Bacon unit test file
-
-require 'geo_ip'
+require_relative '../geo_ip'
 
 # test Numeric.to_octet
-describe 'Numeric.to_octet' do
-  it 'should return an array for 0.to_octet' do
-    0.to_octets.class.to_s.should.equal 'Array'
+class TestNumeric_to_octet < MiniTest::Unit::TestCase
+
+  # do not replace the values
+  def test_constants
+    assert_equal( Geo_IP::MINIMUM_IP, 0          )
+    assert_equal( Geo_IP::MAXIMUM_IP, 4294967295 ) # AKA (256**4 - 1)
   end
 
-  it 'should have four elements for 0.to_octets' do
-    0.to_octets.length.should.equal 4
+  def test_to_octet
+    assert_instance_of( Array, 0.to_octets, "Didn't get an Array back")
+
+    assert_equal( 0.to_octets.length, 4, "The array doesn't have four elements." )
+    assert_equal( Geo_IP::MINIMUM_IP.to_octets, [0, 0, 0, 0],         "When given '#{ Geo_IP::MINIMUM_IP }', didn't receive [0,0,0,0] back." )
+    assert_equal( Geo_IP::MAXIMUM_IP.to_octets, [255, 255, 255, 255], "When given '#{ Geo_IP::MAXIMUM_IP }', didn't receive '[255,255,255,255]' back."  )
+
+    assert_raises(ArgumentError) { (Geo_IP::MINIMUM_IP - 1).to_octets }
+    assert_raises(ArgumentError) { (Geo_IP::MAXIMUM_IP + 1).to_octets }
   end
 
-  it 'should return [0,0,0,0] for 0.to_octets' do
-    0.to_octets.should.equal [0,0,0,0]
+  def test_to_hex_octets
+    assert_equal( Geo_IP::MINIMUM_IP.to_hex_octets, '00.00.00.00' )
+    assert_equal( Geo_IP::MAXIMUM_IP.to_hex_octets, 'ff.ff.ff.ff' )
   end
 
-  it 'should return [255,255,255,255] for 4294967295.to_octets' do
-    4294967295.to_octets.should.equal [255,255,255,255]
+  def test_to_dec_octets
+    assert_equal( Geo_IP::MINIMUM_IP.to_dec_octets, '0.0.0.0'         )
+    assert_equal( Geo_IP::MAXIMUM_IP.to_dec_octets, '255.255.255.255' )
   end
 
-  it 'should raise ArgumentError for < 0.to_octets' do
-    should.raise(ArgumentError) { -1.to_octets }
+  def test_dec_IP_to_value
+    assert_equal( '0.0.0.0'.dec_IP_to_value,         Geo_IP::MINIMUM_IP )
+    assert_equal( '255.255.255.255'.dec_IP_to_value, Geo_IP::MAXIMUM_IP )
   end
 
-  it 'should raise ArgumentError for > 4294967295.to_octets' do
-    should.raise(ArgumentError) { 4294967296.to_octets }
-  end
-end
-
-# test Numeric.to_hex_octets
-describe 'Numeric.to_hex_octets' do
-  it 'should return "00.00.00.00" for 0.to_octets' do
-    0.to_hex_octets.should.equal '00.00.00.00'
+  def test_hex_IP_to_value
+    assert_equal( '0.0.0.0'.hex_IP_to_value,     Geo_IP::MINIMUM_IP )
+    assert_equal( 'ff.ff.ff.ff'.hex_IP_to_value, Geo_IP::MAXIMUM_IP )
   end
 
-  it 'should return "ff.ff.ff.ff" for 4294967295.to_hex_octets' do
-    4294967295.to_hex_octets.should.equal 'ff.ff.ff.ff'
-  end
-end
+  def test_string_is_dec_IP?
+    assert( '0.0.0.0'.is_dec_IP?         )
+    assert( '255.255.255.255'.is_dec_IP? )
 
-# test Numeric.to_dec_octets
-describe '0.to_dec_octets' do
-  it 'should return "0.0.0.0" for 0.to_dec_octets' do
-    0.to_dec_octets.should.equal '0.0.0.0'
-  end
-
-  it 'should return "255.255.255.255" for 4294967295.to_dec_octets' do
-    4294967295.to_dec_octets.should.equal '255.255.255.255'
-  end
-end
-
-# test String.dec_IP_to_value
-describe 'String.dec_IP_to_value' do
-  it 'should return 0 for "0.0.0.0".dec_IP_to_value' do
-    '0.0.0.0'.dec_IP_to_value.should.equal 0
+    assert( ! '0.0.0'.is_dec_IP?     )
+    assert( ! '.0.0.0'.is_dec_IP?    )
+    assert( ! '0.0.0.0.0'.is_dec_IP? )
+    assert( ! '....'.is_dec_IP?      )
+    assert( ! '0'.is_dec_IP?         )
+    assert( ! '-1'.is_dec_IP?        )
   end
 
-  it 'should return 4294967295 for "255.255.255.255".dec_IP_to_value' do
-    "255.255.255.255".dec_IP_to_value.should.equal 4294967295
-  end
-end
+  def test_string_is_hex_IP?
+    assert( '00.00.00.00'.is_hex_IP? )
+    assert( 'ff.ff.ff.ff'.is_hex_IP? )
 
-# test String.hex_IP_to_value
-describe 'String.hex_IP_to_value' do
-  it 'should return 0 for "00.00.00.00".hex_IP_to_value' do
-    '0.0.0.0'.hex_IP_to_value.should.equal 0
+    assert( ! 'ff.ff.ff'.is_hex_IP?       )
+    assert( ! '.ff.ff.ff'.is_hex_IP?      )
+    assert( ! 'ff.ff.ff.ff.ff'.is_hex_IP? )
+    assert( ! '....'.is_hex_IP?           )
+    assert( ! '0'.is_hex_IP?              )
   end
 
-  it 'should return 4294967295 for "ff.ff.ff.ff".hex_IP_to_value' do
-    'ff.ff.ff.ff'.hex_IP_to_value.should.equal 4294967295
-  end
 end
 
