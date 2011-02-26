2ea7ad8f1c932811f50e0d6f9079bc47c21054d3
gpherguson <8si.greg@gmail.com>
2011-02-25 22:19:46 -0700

    updated

--- add_geo_ip_to_db.rb
+++ add_geo_ip_to_db.rb
@@ -6,7 +6,8 @@ require 'iconv'
 require 'rubygems'
 require 'sequel'
 
-require 'geo_ip'
+# use "require" for Ruby < 1.9.2
+require_relative './geo_ip'
 
 LOG_DB = false
 
@@ -19,7 +20,7 @@ def store_geo_ip_to_db(g)
   geo_ip[ :ip_from_hex ] = geo_ip[ :ip_from ].to_hex_octets
   geo_ip[ :ip_to_hex   ] = geo_ip[ :ip_to   ].to_hex_octets
 
-  DB[:geo_ip].insert(geo_ip)
+  DB[:geo_ips].insert(geo_ip)
 rescue Exception => e
   print e
 end
@@ -31,20 +32,38 @@ if (geo_ip_csv.nil?)
   exit
 end
 
-DB = Sequel.connect('postgres://postgres:password@localhost/geo_ip')
+# DB = Sequel.connect('postgres://userid:password@host/geo_ip')
+DB = Sequel.sqlite('sql/geo_ip')
 if (LOG_DB)
   require 'logger'
   DB.loggers << Logger.new(STDOUT)
 end
 
-# Delete the existing records from the database. This might be slower than a
-# SQL truncate.
-DB[:geo_ip].delete
+puts "Truncating the table..."
+DB[:geo_ips].truncate
 
 conv = Iconv.new('UTF-8', 'ISO-8859-1') 
-line_num = 0
 
+# count the number of lines in the file.
+lines_to_read = File.open('IpToCountry.csv','r') { |fi| fi.lines.count }
+puts "#{ lines_to_read } lines to read."
+
+STDOUT.sync = true
+
+line_num = 0
+running = true
+
+# let a timer handle updating the status every second
+timer = Thread.new do
+  while (running) do
+    sleep 1
+    print "%.1f%% %d\r" % [ line_num.to_f / lines_to_read * 100, line_num ]
+  end
+end
+
+# loop over the file and store the records in the database.
 IO.foreach(geo_ip_csv) do |_line| 
+
   line_num += 1
 
   # ignore comments
@@ -53,7 +72,6 @@ IO.foreach(geo_ip_csv) do |_line|
   converted_line = conv.iconv(_line) 
 
   # create a new instance of the object
-  # geo_ip = Geo_IP.new(*CSV.parse_line(_line).map{ |_i| _i.data })
   geo_ip = Geo_IP.new(*CSV.parse_line(converted_line))
 
   begin
@@ -62,13 +80,16 @@ IO.foreach(geo_ip_csv) do |_line|
 
     # show what we got
     # print [geo_ip.ip_from.to_hex_octets, geo_ip.ip_to.to_hex_octets, geo_ip.assigned.strftime('%Y-%m-%d')].join("\t"), "\n"
-  rescue PGError => e
-    $stderr.print "#{ e.msg } at line: #{ line_num }\n"
+  # rescue PGError => e
+  #   $stderr.print "#{ e.msg } at line: #{ line_num }\n"
   rescue Exception => e
     # complain if there is a problem, but don't stop.
     $stderr.print "#{ e.msg } at line: #{ line_num }\n"
   end
 end 
 
-print DB[:geo_ip].count, " records available.\n"
+running = false
+timer.join
+
+print DB[:geo_ips].count, " records available.\n"
 
